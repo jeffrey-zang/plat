01825fbee3920dca8e4856c76177b424308295cc
Gary Zhang <gary.ca.zhang@gmail.com>
2024-01-20 12:37:26 -0500

    feat(fe): set up routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
 import Navbar from "./components/Navbar/Navbar"
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <div className='flex'>
       <Navbar />
-      <h1>Plat</h1>
-      <p>
-        what's up guys plat here 
-        <a>plat</a>
-      </p>
-      <button className=''>What's good</button>
-      <input></input>
+
+      <Routes>
+        <Route index element={
+          <div>
+            <h1>Plat</h1>
+            <p>
+              what's up guys plat here 
+              <a>plat</a>
+            </p>
+            <button className=''>What's good</button>
+            <input></input>
+          </div>
+        } />
+
+
+      </Routes>
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,48 @@
+interface Link {
+  name: string;
+  href: string;
+}
+
 const Navbar = () => {
+
+  let links = [
+    {
+      name: "Plat",
+      href: "/Plat"
+    },
+    {
+      name: "Record",
+      href: "/record"
+    }, 
+    {
+      name: "Upload",
+      href: "/upload"  
+    },
+    {
+      name: "Settings",
+      href: "/settings"  
+    },
+    {
+      name: "Log In",
+      href: "/login"   
+    }
+  ] as Link[]
+  
   return (
-    <div>Navbar</div>
+    <div className='flex flex-col p-8'>
+      <h1>Plat</h1>
+      {
+        links.map(
+          (link, index) => {
+            return (
+              <a key={'navlink-'+index} href={link.href}>
+                {link.name}
+              </a>
+            )
+          }
+        )
+      }
+    </div>
   )
 }
 
