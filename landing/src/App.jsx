3ef22601017be9c9e976ac4fec6c8668d24ee87b
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-02-03 16:49:02 -0500

    feat(landing): create preliminary landing page

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.jpeg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className='grid place-items-center h-screen w-full'>
+      <div className='p-12 px-20 bg-white border-slate-300 border-[1px] rounded-md'>
+        <div className='flex flex-col justify-center items-center gap-8'>
+          <img src={logo} alt='logo' className='h-8' />
+          <h1 className='text-center font-bold text-6xl text-slate-600'>Plat</h1>
+        </div>
+        <p className='text-slate-400 text-center mt-8 max-w-[60%] mx-auto'>
+          Quack on to your next pitch.
         </p>
+        <input placeholder='Email' className='w-full outline-none border-[1px] border-slate-300 p-2 rounded-md mt-8'></input>
+        <button className='w-full outline-none border-[1px] p-2 rounded-md border-slate-300 bg-slate-200 mt-2 hover:opacity-80'>Join the waiting list</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
